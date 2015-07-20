c8383c4f53d7a8e57cb7530d931f6134fac4bab3
Jon M <Retr0@users.noreply.github.com>
2015-07-20 21:54:45 +0100

    link back to blog from app

--- ui.R
+++ ui.R
@@ -8,7 +8,8 @@ shinyUI(fluidPage(
                   uiOutput("instSelector")),
     
     mainPanel(plotOutput(outputId = "plot1"),
-              dataTableOutput(outputId="dt1"))
+              dataTableOutput(outputId="dt1"),
+              h5(tags$a(href="http://retr0.me/2015/07/07/UK-HEI-journal-subscriptions.html", "Blog post with more details")))
   )
 )
 )
